29360b34d636079da00906415c8cd656724f5257
Michelle Suniga <suniga.mrs@gmail.com>
2019-01-05 14:06:41 +0800

    Portfolio v3 as of 01-05-2019

---
+++ dist/index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<title>Portfolio - Draft</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+
+	<!-- google fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700,700i,800" rel="stylesheet">
+
+	<!-- external css -->
+	<link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+	<header>
+		<div id="logo-title">
+			<a href="#">
+				<h1>Michelle Suniga</h1>
+			</a>
+			<p>Full Stack Web Developer</p>
+		</div>
+
+		<nav class="d-none">
+			<div id="my-nav">
+				<ul>
+					<li><a class="nav-hover my-nav-item" href="#">FAQs</a></li>
+					<li><a class="nav-hover my-nav-item" href="#">Contacts</a></li>
+				</ul>
+			</div>
+		</nav>
+
+	</header>
+
+	<main>
+		<section id="masthead">
+			<div id="landing-page-title">
+				<h2 > Hello! Let's make an <span>Impact</span></h2>
+			</div>
+			<div class="container-fluid">
+				<div>
+					<a href="#" class="my-btn-1">See Projects</a>
+				</div>
+			</div>
+		</section>
+
+		<section id="home-content">
+			<div class="container-fluid">
+				<div class="row">
+
+					<div class="col-md-4">
+						<div class="profile-pic">
+							<img src="" alt="Michelle Suniga">
+						</div>
+					</div>
+
+					<div class="col-md-8">
+						<div class="row">
+							<div class="col-md-12">
+								<h3>I am a <span>Full Stack Web Developer</span> and a <span>Graphic Designer</span>.</h3>
+								<p>With a passion for problem-solving and creative use of technology to communicate, Michelle hopes to create
+									positive impact to the people and in every industry.</p>
+							</div>
+						</div>
+
+						<div class="row d-md-none">
+							<div class="col-md-12">
+								<div class="link-left">
+									<a href="#" class="my-btn-2">View Skills</a>
+								</div>
+							</div>
+						</div>
+
+						<div id="skillset" class="row d-none d-md-block"> <!-- REPLACE D-NONE with class to show-->
+							<div class="col-md-12">
+								<div class="row"> 
+									
+									<div class="col-md-12">
+										<h4>Skillset</h4>
+										<div class="row">
+
+											<div class="col-md-3">
+												<ul>
+													<li>HTML5</li>
+													<li>CSS3</li>
+													<li>Bootstrap 4</li>
+													<li>PHP</li>
+													<li>Laravel 5</li>
+												</ul>
+											</div>
+
+											<div class="col-md-3">
+												<ul>
+													<li>Web Design</li>
+													<li>Graphic Design</li>
+													<li>Illustration</li>
+													<li>Branding</li>
+													<li>Art Direction</li>
+												</ul>
+											</div>
+
+											<div class="col-md-3">
+												<ul>
+													<li>Photography</li>
+													<li>Illustration</li>
+													<li>Video Production</li>
+													<li>Video Editing</li>
+												</ul>
+											</div>
+
+										</div>
+									</div>
+								</div>
+
+								<div class="row  d-md-none">
+									<div class="col-md-12">
+										<div class="link-left">
+											<a href="#">Back</a>
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+	</main>
+	
+	<footer class="d-flex justify-content-between ">
+		<div class="align-self-center">
+			<p>Michelle Suniga &copy; 2019</p>
+		</div>
+		<div class="align-self-center">
+			<a href="mailto:suniga.mrs@gmail.com">suniga.mrs@gmail.com</a>
+		</div>
+	</footer>
+
+	<!-- jQuery library -->
+	<script src="vendor/jquery/jquery.min.js"></script>
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+	<!-- Custom JS -->
+	<!-- <script src="assets/js/script.js"></script> -->
+
+</body>
+
+</html>
---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+// Load plugins
+const autoprefixer = require("gulp-autoprefixer");
+const browsersync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const gulp = require("gulp");
+// const header = require("gulp-header");
+const plumber = require("gulp-plumber")
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const pkg = require('./package.json');
+
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function(done) {
+
+  // Bootstrap
+  gulp.src([
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+    .pipe(gulp.dest('./dist/vendor/bootstrap'))
+
+  // Font Awesome 
+  gulp.src([
+      './node_modules/@fortawesome/**/*',
+    ])
+    .pipe(gulp.dest('./dist/vendor'))
+
+  // jQuery
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./dist/vendor/jquery'))
+
+  // jQuery Easing
+  gulp.src([
+      './node_modules/jquery.easing/*.js'
+    ])
+    .pipe(gulp.dest('./dist/vendor/jquery-easing'))
+
+  done();
+
+});
+
+/* gulp.task('copy', function(done) {
+    gulp.src([
+        './src/*.php',
+        './src/views/*.php',
+        './src/views/*.php'
+      ])
+      .pipe(gulp.dest('./dist'))
+
+    done();
+}); */
+
+function css() {
+    return gulp
+      .src("./src/scss/*.scss")
+      .pipe(plumber())
+      .pipe(sass({
+        outputStyle: "expanded"
+      }))
+      .on("error", sass.logError)
+    //   .pipe(autoprefixer({
+    //     browsers: ['last 2 versions'],
+    //     cascade: false
+    //   }))
+    //   .pipe(header(banner, {
+    //     pkg: pkg
+    //   }))
+      .pipe(gulp.dest("./dist/assets/css"))
+      .pipe(rename({
+        suffix: ".min"
+      }))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest("./dist/assets/css"))
+      .pipe(browsersync.stream());
+  }
+  
+
+// Tasks
+gulp.task("css", css);
+
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    proxy: {
+        target: "test.dev"
+    }
+  });
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./src/scss/**/*", css);
+//   gulp.watch(["./js/**/*.js", "!./js/*.min.js"], js);
+  gulp.watch("./dist/**/*.php", browserSyncReload);
+}
+
+gulp.task("default", gulp.parallel('vendor', css));
+
+// dev task
+gulp.task("dev", gulp.parallel(watchFiles, browserSync));
